a4287c0d0fb875269136fe0d4a9afdb03c3ceed6
Omonqulov Jasurbek <omonovjasur021@gmail.com>
2025-05-10 11:31:03 +0500

    summa

--- main.py
+++ main.py
@@ -1,44 +1,42 @@
 import asyncio
 import logging
 import sys
-
+import os
+import wikipedia
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher, html
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
-from aiogram.filters import CommandStart
+from aiogram.filters import Command
 from aiogram.types import Message
 
+
 load_dotenv()
-
-TOKEN = "7834765504:AAEjaFj2qy01hU09q5lYdZGptqi9Fx-vfBQ"
+TOKEN = os.getenv("API")
 dp = Dispatcher()
+print("TOKEN:", TOKEN)
+wikipedia.set_lang("uz")
 
-
-@dp.message(CommandStart())
-async def command_start_handler(message: Message) -> None:
-    await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
+@dp.message(Command(commands=["start"]))
+async def start_handler(message: Message):
+    await message.answer(f"Hello , {html.bold(message.from_user.full_name)}!")
 
 
 @dp.message()
-async def echo_handler(message: Message) -> None:
+async def message_handler(message: Message):
     try:
         await message.send_copy(chat_id=message.chat.id)
-    except TypeError:
-        await message.answer("Nice try!")
+        full =wikipedia.summary(message.text)
+        await message.answer(full)
+    except:
+        await message.answer("Bu mavzu yo`q❌, boshqa yozing❗")
 
 
 async def main():
-    bot = Bot(
-        token=TOKEN,
-        default=DefaultBotProperties(parse_mode=ParseMode.HTML),
-    )
+    bot = Bot(token=TOKEN,default=DefaultBotProperties(parse_mode=ParseMode.HTML),)
     await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
-    logging.basicConfig(
-        level=logging.INFO,
-        stream=sys.stdout,
-    )
+    logging.basicConfig(level=logging.INFO,stream=sys.stdout,)
     asyncio.run(main())
