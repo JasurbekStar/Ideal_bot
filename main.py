74c61d49ff94c48447f8f771b746f6ea01ada0fa
Omonqulov Jasurbek <156973763+Jasurbek3@users.noreply.github.com>
2025-05-10 20:39:36 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,45 +1,27 @@
-import asyncio
-import logging
-import sys
-import wikipedia
+import asyncio, logging, sys, wikipedia
 from aiogram import Bot, Dispatcher, html
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import Command
 from aiogram.types import Message
-
-
 TOKEN = "7834765504:AAEjaFj2qy01hU09q5lYdZGptqi9Fx-vfBQ"
 dp = Dispatcher()
 print("TOKEN:", TOKEN)
-
 wikipedia.set_lang("uz")
-
 @dp.message(Command(commands=["start"]))
 async def start_handler(message: Message):
-    id = message.from_user.id
-    print(id)
     await message.answer(f"Assalomu allekum, {html.bold(message.from_user.full_name)}!")
-
-
 @dp.message()
 async def message_handler(message: Message):
-    id = message.from_user.id
-    print(id)
     try:
         await message.send_copy(chat_id=message.chat.id)
         full =wikipedia.summary(message.text)
         await message.answer(full)
     except:
         await message.answer("Bu mavzu yo`q❌, boshqa yozing❗")
-
-
 async def main():
     bot = Bot(token=TOKEN,default=DefaultBotProperties(parse_mode=ParseMode.HTML),)
     await dp.start_polling(bot)
-
-
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-    logging.info("Bot is starting...")
     asyncio.run(main())
