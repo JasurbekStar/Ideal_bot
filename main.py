72b03f84b48359ebcac25e011ad5b772cccede7d
Omonqulov Jasurbek <156973763+Jasurbek3@users.noreply.github.com>
2025-05-10 07:22:52 +0500

    Update main.py
    full

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
 from aiogram.types import Message
-
 load_dotenv()
 
 TOKEN = "7834765504:AAEjaFj2qy01hU09q5lYdZGptqi9Fx-vfBQ"
